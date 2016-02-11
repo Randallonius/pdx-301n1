13e0e92e40034d8c840e67844dc7f3fa7d2ac89c
Randallonius <jonathanprandall@gmail.com>
2016-02-11 09:29:28 -0800

    checked off TODO's

---
+++ class-14-managing-state/pair-assignment/jonathan/scripts/articleController.js
@@ -0,0 +1,57 @@
+(function(module) {
+  var articlesController = {};
+
+  Article.createTable();  // Ensure the database table is properly initialized
+
+  articlesController.index = function(ctx, next) {
+    articleView.index(ctx.articles);
+  };
+
+  // DONE!: Middleware for grabbing `one` article by ID:
+  articlesController.loadById = function(ctx, next) {
+    var articleData = function(article) {
+      ctx.articles = article;
+      next();
+    };
+
+    Article.findWhere('id', ctx.params.id, articleData);
+  };
+
+  // DONE!: Middleware for loading up articles by a certain author:
+  articlesController.loadByAuthor = function(ctx, next) {
+    var authorData = function(articlesByAuthor) {
+      ctx.articles = articlesByAuthor;
+      next();
+    };
+
+    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+  };
+
+  // DONE!: Middleware for grabbing all articles with a certain category:
+  articlesController.loadByCategory = function(ctx, next) {
+    var categoryData = function(articlesInCategory) {
+      ctx.articles = articlesInCategory;
+      next();
+    };
+
+    Article.findWhere('category', ctx.params.categoryName, categoryData);
+  };
+
+  // DONE!: Middleware for grabbing ALL articles:
+  articlesController.loadAll = function(ctx, next) {
+    var articleData = function(allArticles) {
+      ctx.articles = Article.all;
+      next();
+    };
+
+    if (Article.all.length) {
+      ctx.articles = Article.all;
+      next();
+    } else {
+      Article.fetchAll(articleData);
+    }
+  };
+
+
+  module.articlesController = articlesController;
+})(window);
